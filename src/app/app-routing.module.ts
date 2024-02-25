b8dff33967a97949eb6b28e6f49d732964542fb6
NicolasCasClew <nicolascasclew@gmail.com>
2024-02-25 06:14:10 +0000

    routing DONE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component:ProfileComponent},
+  { path: 'about', component:AboutComponent}
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
