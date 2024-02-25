aee63f366aa06d94bc1ccfa2b63945d199f1de24
NicolasCasClew <nicolascasclew@gmail.com>
2024-02-25 06:09:34 +0000

    ?

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+
+import { AboutComponent } from "./components/about/about.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+
+const routes: Routes = [
+    { path: '', component:ProfileComponent},
+    { path: 'about', component:AboutComponent}
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
+
+/* export const appRouterProviders:any = [];
+export const routing:ModuleWithProviders = RouterModule.forRoot(routes);
+ */
